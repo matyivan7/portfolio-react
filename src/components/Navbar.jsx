fff2ead79a93eef8a0907f355ff2795c965ffa37
ivanm <matyivan@gmail.com>
2025-03-07 12:54:18 +0100

    Language swap icon implemented Mobile view optimised

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,27 @@
 import React, {useState} from 'react'
-import { FaBars, FaTimes } from 'react-icons/fa'
+import {FaBars, FaFlagUsa, FaTimes} from 'react-icons/fa'
 
 const Navbar = () => {
     const [navOpen, setNavOpen] = useState(false)
+    const [langOpen, setLangOpen] = useState(false);
+    const [language, setLanguage] = useState('English');
+    const flags = {
+        'English': '/flags/uk.png',
+        'Magyar': '/flags/hungary.png'
+    };
 
     const handleToggle = () => {
         setNavOpen(!navOpen)
     }
+
+    const toggleLangMenu = () => {
+        setLangOpen(!langOpen);
+    }
+
+    const changeLanguage = (lang) => {
+        setLanguage(lang);
+        setLangOpen(false);
+    };
 
     return (
         <nav className="fixed top-0 left-0 right-0 bg-gray-900 text-white z-50">
@@ -14,7 +29,7 @@ const Navbar = () => {
 
                 <div className="flex items-center">
                     <a href="#hero" className="flex items-center">
-                        <img src="/m-icon.png" alt="Logo" className="w-10 h-10 mr-2" />
+                        <img src="/m-icon.png" alt="Logo" className="w-10 h-10 mr-2"/>
                     </a>
                 </div>
 
@@ -25,13 +40,32 @@ const Navbar = () => {
                     <a href="#contact" className="hover:text-gray-300">Contact</a>
                 </div>
 
-
+                <div className="relative">
+                    <button
+                        onClick={toggleLangMenu}
+                        className="flex items-center text-sm px-4 py-2 rounded-lg bg-gray-800 hover:bg-gray-700">
+                        <img src={flags[language]} alt={language} className="w-5 h-5 mr-2" />
+                        {language}
+                    </button>
+                    {langOpen && (
+                        <div className="absolute right-0 mt-2 w-40 bg-gray-200 text-black shadow-lg rounded-md">
+                            <button onClick={() => changeLanguage('English')} className="flex items-center px-4 py-2 rounded-lg hover:bg-gray-300 w-full text-left">
+                                <img src={flags['English']} alt="English" className="w-5 h-5 mr-2" />
+                                English
+                            </button>
+                            <button onClick={() => changeLanguage('Magyar')} className="flex items-center px-4 py-2 rounded-lg hover:bg-gray-300 w-full text-left">
+                                <img src={flags['Magyar']} alt="Magyar" className="w-5 h-5 mr-2" />
+                                Magyar
+                            </button>
+                        </div>
+                    )}
+                </div>
                 <button
                     className="md:hidden text-2xl focus:outline-none"
                     onClick={handleToggle}
                     aria-label="Toggle Menu"
                 >
-                    {navOpen ? <FaTimes /> : <FaBars />}
+                    {navOpen ? <FaTimes/> : <FaBars/>}
                 </button>
             </div>
 
@@ -65,7 +99,7 @@ const Navbar = () => {
                     >
                         Contact
                     </a>
-            </div>
+                </div>
             )}
         </nav>
     )
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
-import {FaDatabase, FaLaptopCode, FaMobileAlt, FaPaintBrush, FaRocket} from 'react-icons/fa'
+import React, {useEffect, useState} from 'react'
+import {FaAngleDown, FaAngleUp, FaDatabase, FaLaptopCode, FaMobileAlt, FaPaintBrush, FaRocket} from 'react-icons/fa'
 import {FaGear} from "react-icons/fa6";
 
 const Services = () => {
+
+    const [showMore, setShowMore] = useState(false)
+    const [isMobile, setIsMobile] = useState(false)
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsMobile(window.innerWidth < 768)
+        }
+
+        handleResize()
+        window.addEventListener('resize', handleResize)
+        return () => window.removeEventListener('resize', handleResize)
+    }, [])
+
     return (
         <section
             id="services"
@@ -14,7 +28,7 @@ const Services = () => {
                 </h2>
                 <div className="grid md:grid-cols-3 gap-8">
                     <div className="bg-gray-100 shadow p-6 rounded-lg text-center">
-                        <FaLaptopCode className="mx-auto text-4xl text-green-500 mb-4" />
+                        <FaLaptopCode className="mx-auto text-4xl text-green-500 mb-4"/>
                         <h3 className="text-xl font-semibold mb-2">Frontend Development</h3>
                         <p className="text-gray-700">
                             Skilled in both Angular and React, with good knowledge in JavaScript and TypeScript.
@@ -24,7 +38,7 @@ const Services = () => {
                     </div>
 
                     <div className="bg-gray-100 shadow p-6 rounded-lg text-center">
-                        <FaGear className="mx-auto text-4xl text-green-500 mb-4" />
+                        <FaGear className="mx-auto text-4xl text-green-500 mb-4"/>
                         <h3 className="text-xl font-semibold mb-2">Backend Development</h3>
                         <p className="text-gray-700">
                             Experienced in building robust backend systems with Spring Boot and RESTful APIs in Java.
@@ -34,7 +48,7 @@ const Services = () => {
                     </div>
 
                     <div className="bg-gray-100 shadow p-6 rounded-lg text-center">
-                        <FaMobileAlt className="mx-auto text-4xl text-green-500 mb-4" />
+                        <FaMobileAlt className="mx-auto text-4xl text-green-500 mb-4"/>
                         <h3 className="text-xl font-semibold mb-2">Apps Development</h3>
                         <p className="text-gray-700">
                             Previously worked with React Native for mobile development and currently expanding expertise
@@ -43,35 +57,50 @@ const Services = () => {
                     </div>
                 </div>
 
-                <div className="grid md:grid-cols-3 gap-8 mt-12">
-                    <div className="bg-gray-100 shadow p-6 rounded-lg text-center">
-                        <FaDatabase className="mx-auto text-4xl text-green-500 mb-4" />
-                        <h3 className="text-xl font-semibold mb-2">Database Management</h3>
-                        <p className="text-gray-700">
-                            Proficient in MySQL, PostgreSQL, and Oracle databases, with hands-on experience in writing
-                            optimized SQL queries for efficient data management and retrieval
-                        </p>
-                    </div>
+                {(!isMobile || showMore) && (
+                    <div className="grid md:grid-cols-3 gap-8 mt-12">
+                        <div className="bg-gray-100 shadow p-6 rounded-lg text-center">
+                            <FaDatabase className="mx-auto text-4xl text-green-500 mb-4"/>
+                            <h3 className="text-xl font-semibold mb-2">Database Management</h3>
+                            <p className="text-gray-700">
+                                Proficient in MySQL, PostgreSQL, and Oracle databases, with hands-on experience in
+                                writing
+                                optimized SQL queries for efficient data management and retrieval
+                            </p>
+                        </div>
 
-                    <div className="bg-gray-100 shadow p-6 rounded-lg text-center">
-                        <FaPaintBrush className="mx-auto text-4xl text-green-500 mb-4" />
-                        <h3 className="text-xl font-semibold mb-2">UI/UX Design</h3>
-                        <p className="text-gray-700">
-                            Capable of designing modern, visually appealing, and interactive websites with smooth
-                            animations and an engaging user experience that aligns with the latest design trends
-                        </p>
-                    </div>
+                        <div className="bg-gray-100 shadow p-6 rounded-lg text-center">
+                            <FaPaintBrush className="mx-auto text-4xl text-green-500 mb-4"/>
+                            <h3 className="text-xl font-semibold mb-2">UI/UX Design</h3>
+                            <p className="text-gray-700">
+                                Capable of designing modern, visually appealing, and interactive websites with smooth
+                                animations and an engaging user experience that aligns with the latest design trends
+                            </p>
+                        </div>
 
-                    <div className="bg-gray-100 shadow p-6 rounded-lg text-center">
-                        <FaRocket className="mx-auto text-4xl text-green-500 mb-4" />
-                        <h3 className="text-xl font-semibold mb-2">Performance Optimization</h3>
-                        <p className="text-gray-700">
-                            Focused on improving application speed and efficiency by optimizing code, using OOP
-                            programming principles reducing load times, and ensuring smooth functionality across
-                            different devices and platforms
-                        </p>
+                        <div className="bg-gray-100 shadow p-6 rounded-lg text-center">
+                            <FaRocket className="mx-auto text-4xl text-green-500 mb-4"/>
+                            <h3 className="text-xl font-semibold mb-2">Performance Optimization</h3>
+                            <p className="text-gray-700">
+                                Focused on improving application speed and efficiency by optimizing code, using OOP
+                                programming principles reducing load times, and ensuring smooth functionality across
+                                different devices and platforms
+                            </p>
+                        </div>
                     </div>
-                </div>
+                )}
+
+                {isMobile && (
+                    <div className="flex justify-center mt-4">
+                        <button onClick={() => setShowMore(!showMore)}>
+                            {showMore ? (
+                                <FaAngleUp className="text-5xl text-green-400"/>
+                            ) : (
+                                <FaAngleDown className="text-5xl text-green-400"/>
+                            )}
+                        </button>
+                    </div>
+                )}
             </div>
         </section>
     )
