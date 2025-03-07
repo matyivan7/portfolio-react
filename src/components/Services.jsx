c9e630b18d8e7b788cb9615008ae36d551d1cdcf
ivanm <matyivan@gmail.com>
2025-03-07 09:21:44 +0100

    About me captions

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -13,48 +13,63 @@ const Services = () => {
                     Crafting fun and interactive websites and apps
                 </h2>
                 <div className="grid md:grid-cols-3 gap-8">
-                    <div className="bg-white shadow p-6 rounded-lg text-center">
+                    <div className="bg-gray-100 shadow p-6 rounded-lg text-center">
                         <FaLaptopCode className="mx-auto text-4xl text-green-500 mb-4" />
                         <h3 className="text-xl font-semibold mb-2">Frontend Development</h3>
                         <p className="text-gray-700">
-                            text
+                            Skilled in both Angular and React, with good knowledge in JavaScript and TypeScript.
+                            Proficient in building dynamic and responsive web applications using HTML, CSS, and SCSS
+                            to create seamless user experiences
                         </p>
                     </div>
 
-                    <div className="bg-white shadow p-6 rounded-lg text-center">
+                    <div className="bg-gray-100 shadow p-6 rounded-lg text-center">
                         <FaGear className="mx-auto text-4xl text-green-500 mb-4" />
                         <h3 className="text-xl font-semibold mb-2">Backend Development</h3>
                         <p className="text-gray-700">
-                            text
+                            Experienced in building robust backend systems with Spring Boot and RESTful APIs in Java.
+                            Additionally, proficient in Python for backend development, ensuring scalable and efficient
+                            server-side solutions
                         </p>
                     </div>
 
-                    <div className="bg-white shadow p-6 rounded-lg text-center">
+                    <div className="bg-gray-100 shadow p-6 rounded-lg text-center">
                         <FaMobileAlt className="mx-auto text-4xl text-green-500 mb-4" />
                         <h3 className="text-xl font-semibold mb-2">Apps Development</h3>
                         <p className="text-gray-700">
-                            text
+                            Previously worked with React Native for mobile development and currently expanding expertise
+                            in Flutter to create cross-platform applications with a smooth and intuitive user experience
                         </p>
                     </div>
                 </div>
 
                 <div className="grid md:grid-cols-3 gap-8 mt-12">
-                    <div className="bg-white shadow p-6 rounded-lg text-center">
+                    <div className="bg-gray-100 shadow p-6 rounded-lg text-center">
                         <FaDatabase className="mx-auto text-4xl text-green-500 mb-4" />
                         <h3 className="text-xl font-semibold mb-2">Database Management</h3>
-                        <p className="text-gray-700">text</p>
+                        <p className="text-gray-700">
+                            Proficient in MySQL, PostgreSQL, and Oracle databases, with hands-on experience in writing
+                            optimized SQL queries for efficient data management and retrieval
+                        </p>
                     </div>
 
-                    <div className="bg-white shadow p-6 rounded-lg text-center">
+                    <div className="bg-gray-100 shadow p-6 rounded-lg text-center">
                         <FaPaintBrush className="mx-auto text-4xl text-green-500 mb-4" />
                         <h3 className="text-xl font-semibold mb-2">UI/UX Design</h3>
-                        <p className="text-gray-700">text</p>
+                        <p className="text-gray-700">
+                            Capable of designing modern, visually appealing, and interactive websites with smooth
+                            animations and an engaging user experience that aligns with the latest design trends
+                        </p>
                     </div>
 
-                    <div className="bg-white shadow p-6 rounded-lg text-center">
+                    <div className="bg-gray-100 shadow p-6 rounded-lg text-center">
                         <FaRocket className="mx-auto text-4xl text-green-500 mb-4" />
                         <h3 className="text-xl font-semibold mb-2">Performance Optimization</h3>
-                        <p className="text-gray-700">text</p>
+                        <p className="text-gray-700">
+                            Focused on improving application speed and efficiency by optimizing code, using OOP
+                            programming principles reducing load times, and ensuring smooth functionality across
+                            different devices and platforms
+                        </p>
                     </div>
                 </div>
             </div>
